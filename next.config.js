af8a485d91cf49f1e15a398ecfb98d1c1bc3dfb3
francisamidu <francisamidu124@gmail.com>
2022-05-14 02:29:26 +0200

    Update

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   images: {
     domains: ["gateway.ipfs.io", "ipfs.infura.io", "localhost", "*"],
   },
+  publicRuntimeConfig:{
+    INFURA_ID:process.env.NEXT_PUBLIC_INFURA_ID
+  }
 };
